34562481b890207a976deff1841f996937bb03fe
shalash23 <103309490+shalash23@users.noreply.github.com>
2023-01-22 22:12:27 +0200

    first commit

---
+++ src/script.js
@@ -0,0 +1,44 @@
+import "./style.css";
+import * as THREE from "three";
+
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
+
+// Scene
+const scene = new THREE.Scene();
+
+// Object
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+// Sizes
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+
+const clock = new THREE.Clock();
+console.log(Date.now());
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+  mesh.position.y = Math.sin(elapsedTime);
+  camera.position.x = Math.tan(elapsedTime);
+  window.requestAnimationFrame(tick);
+  renderer.render(scene, camera);
+};
+
+tick();
